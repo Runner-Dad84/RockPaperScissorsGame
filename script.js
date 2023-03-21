c30176ef287761467092e71886fd6cf44e43b3a4
Runner-Dad84 <vanhoogie@gmail.com>
2023-03-20 21:35:24 -0400

    Display result and score by DOM textContent

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-//Randomly select computer choice
+//Global variables
+let computerScore = 0;
+let playerScore = 0;
+
 let computerSelection;
 let playerSelection;
 
+//Randomly select computer choice
 function getComputerChoice () {
     computerSelection = Math.floor(Math.random() * 100);
  
@@ -15,6 +19,63 @@ function getComputerChoice () {
     return computerSelection;
 }
 
+let result = document.getElementById("result");
+let score = document.getElementById("score");
+
+//Play One Round function
+function playRound(playerSelection, getComputerChoice) {
+    switch (true) {
+        case playerSelection === "rock" && computerSelection === "scissors":
+            playerScore +=1
+            result.textContent="Rock beats scissors, you win!"
+            score.textContent=`Player = ${playerScore} vs. the Computer = ${computerScore}`
+            break;
+        case playerSelection === "rock" && computerSelection === "paper":
+            computerScore +=1
+            result.textContent="Rock is beat by paper, you lose!"
+            score.textContent=`Player = ${playerScore} vs. the Computer = ${computerScore}`
+            break;
+        case playerSelection === "paper" && computerSelection === "rock":
+            playerScore +=1
+            result.textContent="Paper beats rock, you win!"
+            score.textContent=`Player = ${playerScore} vs. the Computer = ${computerScore}`
+            break;
+        case playerSelection === "paper" && computerSelection === "scissors":
+            computerScore +=1
+            result.textContent="Paper is beat by scissors, you lose!"
+            score.textContent=`Player = ${playerScore} vs. the Computer = ${computerScore}`
+            break;
+        case playerSelection === "scissors" && computerSelection === "paper":
+            playerScore +=1
+            result.textContent="Scissors beats paper, you win!"
+            score.textContent=`Player = ${playerScore} vs. the Computer = ${computerScore}`
+            break;
+        case playerSelection === "scissors" && computerSelection === "rock":
+            computerScore +=1
+            result.textContent="Scissors is beat by rock, you lose!"
+            score.textContent=`Player = ${playerScore} vs. the Computer ${computerScore}`
+            break;
+        case playerSelection === computerSelection:
+            result.textContent="It is a tie!"
+            score.textContent=`Player = ${playerScore} vs. the Computer ${computerScore}`
+            break;
+        default:
+            return(result.textContent="Please enter rock, paper or scissors");
+    }
+}
+
+section.addEventListener("click", event => {
+    playerSelection = event.target.id;
+    getComputerChoice();
+    console.log(playRound(playerSelection, getComputerChoice));
+
+    if (playerScore ===3) {
+        alert("You win!!!"); computerScore = 0; playerScore = 0} 
+    else if (computerScore ===3)  {
+        alert("You lose!!"); computerScore = 0; playerScore = 0} 
+});
+
+
 /*Ask for player choice selection and make case insensitive (lowercase)
 let playerSelection;
 
@@ -24,46 +85,6 @@ function Input() {
     return playerSelection;
 }
 */
-
-//Global Score Variables
-let computerScore = 0;
-let playerScore = 0;
-
-//Play One Round function
-function playRound(playerSelection, getComputerChoice) {
-    switch (true) {
-        case playerSelection === "rock" && computerSelection === "scissors":
-            playerScore +=1
-            console.log("Rock beats scissors, you win!")
-            break;
-        case playerSelection === "rock" && computerSelection === "paper":
-            computerScore +=1
-            console.log("Rock is beat by paper, you lose!")
-            break;
-        case playerSelection === "paper" && computerSelection === "rock":
-            playerScore +=1
-            console.log("Paper beats rock, you win!")
-            break;
-        case playerSelection === "paper" && computerSelection === "scissors":
-            computerScore +=1
-            console.log("Paper is beat by scissors, you lose!")
-            break;
-        case playerSelection === "scissors" && computerSelection === "paper":
-            playerScore +=1
-            console.log("Scissors beats paper, you win!")
-            break;
-        case playerSelection === "scissors" && computerSelection === "rock":
-            computerScore +=1
-            console.log("Scissors is beat by rock, you lose!")
-            break;
-        case playerSelection === computerSelection:
-            console.log("It is a tie!")
-            break;
-        default:
-            return("Please enter rock, paper or scissors");
-    }
-}
-
 /*Play Game
 function game () {
     computerScore = 0;
@@ -80,16 +101,3 @@ function game () {
     };
 }
 */
-
-buttons = document.querySelector('#section');
-
-section.addEventListener("click", event => {
-    playerSelection = event.target.id;
-    getComputerChoice();
-    console.log(playRound(playerSelection, getComputerChoice));
-
-    if (playerScore ===3) {
-        alert("You win!!!"); computerScore = 0; playerScore = 0} 
-    else if (computerScore ===3)  {
-        alert("You lose!!"); computerScore = 0; playerScore = 0} 
-});
